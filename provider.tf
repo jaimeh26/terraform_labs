56fd4835624a74bc7136b1e1cd08df85e5cf53e7
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-09 22:48:25 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,11 @@ terraform{
   required_version=">=1.4.0"#this version is for Terraform Version, NOT azurerm
 }
 
+resource "azurerm_resource_group" "terraform_labs_rg" {
+  name     = "terraform-labs"
+  location = "canadacentral"
+}
+
 provider "azurerm"{
   features{}
   skip_provider_registration="true"
