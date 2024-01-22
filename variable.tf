c716300bc927d0947caf36c9173befad29a0ac49
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-22 18:58:33 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "AWS_ACCESS_KEY"{
 variable "AWS_SECRET_ACCESS_KEY"{
 }
 variable "aws_region" {
-  descrition = "Region in which AWS Resources to be created"
+  description = "Region in which AWS Resources to be created"
   type = string
   default = "us-east-1"
 }
