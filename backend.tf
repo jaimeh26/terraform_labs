d1408accdbacad682eeb7b9c9d5c4edebed55a14
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-22 19:54:57 -0500

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform{
+  required_providers {
+    aws={
+      source = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+    cloud {
+        organization = "jaimeh26"
+      workspaces {
+        name = "terraform_labs"
+      }
+    }
+
+}
