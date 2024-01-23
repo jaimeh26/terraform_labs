b37a3a2a85c60ef2baebaed2e8c386144debd114
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-22 20:49:47 -0500

    Update ec2_spot.tf

--- ec2_spot.tf
+++ ec2_spot.tf
@@ -3,8 +3,8 @@ resource "aws_spot_instance_request" "spotmcit" {
   ami           = "ami-1234"
   spot_price    = "0.03"
   instance_type = "c4.xlarge"
- 
+  count = 3
   tags = {
-    Name = "spotmcit"
+    Name = "spotmcit-${count.index}"
   }
 }
