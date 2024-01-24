7cab437d31583e842f99e9ed665ed1bcd5efd9ad
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-24 18:45:36 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   common_tags = {
-    Owner       = "YourName"   # Update with the appropriate owner name
+    Owner       = "mcitcloud2"   # Update with the appropriate owner name
     Environment = "Production"  # Update with the appropriate environment
     # Add any other common tags as needed
   }
