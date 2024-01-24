59b80ef3dfc10ab52f5a704904ec40ae20ab89c6
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-24 18:24:29 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -18,3 +18,8 @@ variable "vpc_create_database_subnet_group" {
   type        = bool
   default     = false  
 }
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string  
+  default     = "10.0.0.0/16"  
+}
