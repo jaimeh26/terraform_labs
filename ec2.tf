b826cdd64198759d66bdf0df94cd220c1eb6da81
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-24 20:47:19 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  instance_type          = var.instance_type
+  instance_type          = "t2.micro"
   key_name               = "user1"
   monitoring             = true
   vpc_security_group_ids = ["sg-12345678"]
